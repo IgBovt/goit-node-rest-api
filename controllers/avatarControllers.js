dc54f173d637f40489a58424bb5b9cac045b20f2
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-05-18 23:15:01 +0300

    made get/add avatars

--- controllers/avatarControllers.js
+++ controllers/avatarControllers.js
@@ -1,6 +1,41 @@
+import User from "../models/user.js";
+import * as fs from "node:fs/promises";
+import path from "node:path";
+
 export const addAvatar = async (req, res, next) => {
   try {
-    res.send("AVATAR");
+    console.log(req.file);
+    await fs.rename(
+      req.file.path,
+      path.resolve("public/avatars", req.file.filename)
+    );
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      {
+        avatarURL: req.file.filename,
+      },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).send({ message: "Not found" });
+    }
+    res.send(user);
+  } catch (error) {
+    next();
+  }
+};
+
+export const getAvatar = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).send({ message: "Not found" });
+    }
+    if (!user.avatarURL) {
+      return res.status(404).send({ message: "Not found" });
+    }
+    res.sendFile(path.resolve("public/avatars", user.avatarURL));
   } catch (error) {
     next();
   }
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addAvatar } from "../controllers/avatarControllers.js";
+import { addAvatar, getAvatar } from "../controllers/avatarControllers.js";
 import { uploadMiddleware } from "../middleware/upload.js";
 const avatarRouter = express.Router();
 
+avatarRouter.get("/", getAvatar);
 avatarRouter.patch("/", uploadMiddleware.single("avatar"), addAvatar);
 
 export default avatarRouter;
